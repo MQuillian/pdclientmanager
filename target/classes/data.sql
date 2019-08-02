082513b8e4af3c79df0578f03dd8798435f05ad4
Matt Quillian <matt.quillian@gmail.com>
2019-08-02 11:44:49 -0400

    Updated SQL scripts

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,7 +1,6 @@
 USE `walton_public_defender` ;
 
 # Insert investigators
-
 INSERT INTO investigators
 	VALUES
     (1, 'Tim Loodle', 0),
@@ -9,7 +8,6 @@ INSERT INTO investigators
     (3, 'Debbie Downer', 1);
 
 # Insert attorneys
-
 INSERT INTO attorneys
 	VALUES
     (1, 'Matt Quillian', 0, 1),
@@ -18,7 +16,6 @@ INSERT INTO attorneys
     (4, 'Matt Schneider', 1, 2);
 
 # Insert clients
-
 INSERT INTO clients
 	VALUES
     (1, 'Eric Hoefle', 0),
@@ -40,24 +37,22 @@ INSERT INTO judges
     (2, 'John Mott');
     
 # Insert cases
-
 INSERT INTO cases
 	VALUES
-    (1, '18J161450', 1, 0, 1, 1),
-    (2, '18J141738', 2, 0, 1, 2),
-    (3, '18J171429', 3, 1, 1, 1),
-    (4, '17J283757', 4, 0, 1, 2),
-    (5, '17J172365', 5, 0, 1, 2),
-    (6, '18J179254', 6, 0, 1, 1),
-    (7, '18J172979', 6, 1, 1, 1),
-    (8, '17J281737', 7, 0, 1, 2),
-    (9, '18J218436', 7, 0, 1, 2),
-    (10, '18J476421', 8, 1, 1, 1),
-    (11, '17J742874', 9, 0, 1, 2),
-    (12, '18J851936', 10, 0, 1, 1);
+    (1, '18J161450', 1, 1, '2018-7-16', null, 1, 1),
+    (2, '18J141738', 2, 1, '2018-5-14', null, 2, 2),
+    (3, '18J171429', 3, 1, '2018-8-23', null, 1, 1),
+    (4, '17J283757', 4, 1, '2017-10-17', null, 2, 2),
+    (5, '17J172365', 5, 1, '2017-8-16', null, 2, 2),
+    (6, '18J179254', 6, 2, '2018-8-20', '2018-9-17', 1, 1),
+    (7, '18J172979', 6, 2, '2018-6-7', '2018-10-20', 1, 1),
+    (8, '17J281737', 7, 2, '2018-7-16', '2018-1-8', 2, 2),
+    (9, '18J218436', 7, 2, '2018-7-16', '2019-2-5', 2, 1),
+    (10, '18J476421', 8, 1, '2018-3-29', null, 1, 2),
+    (11, '17J742874', 9, 1, '2017-7-20', null, 2, 2),
+    (12, '18J851936', 10, 1, '2018-11-5', null, 1, 1);
 
 # Insert charges
-
 INSERT INTO charges
 	VALUES
 	(1, 'Simple battery', '16-5-23.1'),
@@ -66,20 +61,19 @@ INSERT INTO charges
 	(4, 'Driving while license suspended', '40-5-21'),
 	(5, 'Possession of controlled substance', '16-13-30');
 
-# Insert charged counts
-
-INSERT INTO charged_counts
+# Insert counts
+INSERT INTO counts
 	VALUES
-    (1, 1),
-    (2, 3),
-    (3, 4),
-    (3, 5),
-	(4, 3),
-	(5, 2),
-	(6, 4),
-	(7, 1),
-	(8, 5),
-	(9, 2),
-	(10, 1),
-	(11, 3),
-	(12, 5);
+    (1, 1, 1, 1),
+    (2, 1, 2, 3),
+    (3, 1, 3, 4),
+    (4, 2, 3, 5),
+	(5, 1, 4, 3),
+	(6, 1, 5, 2),
+	(7, 1, 6, 4),
+	(8, 1, 7, 1),
+	(9, 1, 8, 5),
+	(10, 1, 9, 2),
+	(11, 1, 10, 1),
+	(12, 1, 11, 3),
+	(13, 1, 12, 5);
